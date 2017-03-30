7d62be7a1ea54e279675a6706db8648a731b519c
NadegeH <nadege.hamann@hotmail.fr>
2017-03-30 09:22:14 +0200

    débug JS menu

--- js/js.js
+++ js/js.js
@@ -1,12 +1,30 @@
 $(document).ready(function () {
 	
 	$(".menuBurger").click(function () {
+		$("a.parent + ul").hide();
 		$("header nav > ul.flex-container").slideToggle();
 	});
 	
-	$("a.parent").click(function (e) {
-		e.preventDefault();
-		$(this).next("ul").slideToggle();
+	if ($("header nav a").siblings("ul").length > 0) {
+		$(this).addClass("parent");
+	}
+	
+	var vw = verge.viewportW();
+	$(window).resize(function () {
+		vw = verge.viewportW();
+		if (vw >= 768){
+			$("header nav > ul.flex-container").css("display", "flex");
+		}
+		else{
+			$("header nav > ul.flex-container").hide();
+			$("a.parent + ul").show();
+		}
 	});
-	  
+
+	$("a.parent").click(function (e) {
+		if (vw <= 768) {
+			e.preventDefault();
+			$(this).next("ul").slideToggle();
+		}
+	});
 });
