7bc0d0f3fbb2f061493bb882580aeae286f04836
NadegeH <nadege.hamann@hotmail.fr>
2017-03-30 10:09:38 +0200

    ajout class parent via js

--- js/js.js
+++ js/js.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
 	
 	$(".menuBurger").click(function () {
-		$("a.parent + ul").hide();
+		$("li.parent ul").hide();
 		$("header nav > ul.flex-container").slideToggle();
 	});
 	
-	if ($("header nav a").siblings("ul").length > 0) {
-		$(this).addClass("parent");
-	}
+	$("li").has("ul").addClass("parent");
 	
 	var vw = verge.viewportW();
 	$(window).resize(function () {
@@ -17,11 +15,11 @@ $(document).ready(function () {
 		}
 		else{
 			$("header nav > ul.flex-container").hide();
-			$("a.parent + ul").show();
+			$("li.parent ul").show();
 		}
 	});
 
-	$("a.parent").click(function (e) {
+	$("li.parent > a").click(function (e) {
 		if (vw <= 768) {
 			e.preventDefault();
 			$(this).next("ul").slideToggle();
